67a2d946221b88a803cd2045ec02b4ab1430a21b
Marcus Ching <marcuscny00@gmail.com>
2025-02-26 23:56:29 +0800

    submission commit

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,31 +3,38 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+filepath = 'medical_examination.csv'
+
+df = pd.read_csv(filepath)
 
 # 2
-df['overweight'] = None
+# def bmi(height,weight):
+#     bmi_value = weight/(height**2)
+#     if bmi_value > 25:
+#         return 1
+#     else:
+#         return 0
+df['overweight'] = np.where(df['weight']/(np.square(df['height']/100))>25,1,0)
 
 # 3
-
-
+df['cholesterol'] = np.where(df['cholesterol']>1,1,0)
+df['gluc'] = np.where(df['gluc']>1,1,0)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df,value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
 
 
     # 6
-    df_cat = None
-    
+    df_cat = pd.melt(df,id_vars = 'cardio',value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
+    df_cat = df_cat.groupby(['cardio','variable', 'value']).size().reset_index(name='total')
+    # df_cat = df_cat.rename(columns={"variable":"feature"})
 
     # 7
-
-
+    fig = sns.catplot(x = 'variable', y = 'total', hue = 'value',col = 'cardio', data = df_cat, kind = 'bar')
 
     # 8
-    fig = None
+    # fig = None
 
 
     # 9
@@ -38,21 +45,26 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df.loc[(df['ap_lo'] <= df['ap_hi']) & 
+                     (df['height'] >= df['height'].quantile(0.025)) &
+                     (df['height'] <= df['height'].quantile(0.975)) &
+                     (df['weight'] >= df['weight'].quantile(0.025)) &
+                     (df['weight'] <= df['weight'].quantile(0.975))                     
+                     ]
 
     # 12
-    corr = None
+    corr = df_heat.corr().round(1)
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype = bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
+    sns.heatmap(corr, mask=mask, annot = True, cmap='coolwarm', square=True ,vmin = -0.1, vmax = 0.25)
 
 
     # 16
